47a13231f3bd0c7a057965c9ec5420112a59fc9d
EunseokGo <gomonk3037@gmail.com>
2018-06-25 02:22:31 +0900

    Redirect to route '/login' when logout

--- src/views/Auth.js
+++ src/views/Auth.js
@@ -1,23 +1,34 @@
 // eslint-disable-next-line
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import { inject, observer } from 'mobx-react';
 
+@withRouter
 @inject('authStore', 'userStore')
 @observer
 class Auth extends Component {
+  constructor() {
+    super();
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  handleLogout() {
+    this.props.authStore
+      .logout()
+      .then(() => this.props.history.replace('/login'));
+  }
+
   render() {
     const {
       renderLoggedIn = () => null,
       renderLoggedOut = () => null,
-      userStore,
-      authStore
+      userStore
     } = this.props;
 
     const { currentUser } = userStore;
-    const { logout } = authStore;
 
     return currentUser
-      ? renderLoggedIn(currentUser, logout)
+      ? renderLoggedIn(currentUser, this.handleLogout)
       : renderLoggedOut();
   }
 }
