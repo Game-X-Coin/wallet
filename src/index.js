44f49b5f62057b50cffd60899154fe0f4f52cdc2
EunseokGo <gomonk3037@gmail.com>
2018-06-25 20:02:47 +0900

    Add material design icons

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import stores from './stores';
 
 import App from './App';
 
+import 'material-design-icons/iconfont/material-icons.css';
 import 'bootstrap/scss/bootstrap.scss';
 import './styles/main.scss';
 
