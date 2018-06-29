128e64be7a0cfcec6ba1f25780e6238907bc679d
EunseokGo <gomonk3037@gmail.com>
2018-06-29 15:24:02 +0900

    Fix unexpected re-render by extend PureComponent

--- src/components/LoadingSpinner/index.js
+++ src/components/LoadingSpinner/index.js
@@ -1,63 +1,65 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import ReactDOM from 'react-dom';
 import classNames from 'classnames';
 
 import './style.scss';
 
-const LoadingSpinner = props => {
-  const circleSize = 6;
-  const circleWidth = 3;
-  const logoSize = 4;
+class LoadingSpinner extends PureComponent {
+  render() {
+    const circleSize = 6;
+    const circleWidth = 3;
+    const logoSize = 4;
 
-  const Element = ({ className, scale = 1 }) => (
-    <div className={classNames('loading-spinner', className)}>
-      <div
-        className="loading-spinner__circle"
-        style={{
-          width: `${circleSize * scale}rem`,
-          height: `${circleSize * scale}rem`,
-          borderTopWidth: `${circleWidth * scale}px`
-        }}
-      />
-
-      <svg
-        className="loading-spinner__logo"
-        viewBox="0 0 164 133"
-        style={{
-          width: `${logoSize * scale}rem`,
-          height: `${logoSize * scale}rem`
-        }}
-      >
-        <polygon fill="#FFD111" points="41,123 82,82 0,82 " />
-        <rect
-          x="53.009"
-          y="12.009"
-          transform="matrix(0.7071 0.7071 -0.7071 0.7071 53.0087 -45.9742)"
-          fill="#0070BE"
-          width="57.983"
-          height="57.983"
+    const Element = ({ className, scale = 1 }) => (
+      <div className={classNames('loading-spinner', className)}>
+        <div
+          className="loading-spinner__circle"
+          style={{
+            width: `${circleSize * scale}rem`,
+            height: `${circleSize * scale}rem`,
+            borderTopWidth: `${circleWidth * scale}px`
+          }}
         />
-        <rect
-          x="94.009"
-          y="53.009"
-          transform="matrix(0.7071 0.7071 -0.7071 0.7071 94.0088 -62.957)"
-          fill="#33BA20"
-          width="57.983"
-          height="57.983"
-        />
-        <polygon fill="#FF4B34" points="0,82 82,82 41,41 " />
-      </svg>
-    </div>
-  );
 
-  if (props.global) {
-    return ReactDOM.createPortal(
-      <Element className="global" />,
-      document.querySelector('body')
+        <svg
+          className="loading-spinner__logo"
+          viewBox="0 0 164 133"
+          style={{
+            width: `${logoSize * scale}rem`,
+            height: `${logoSize * scale}rem`
+          }}
+        >
+          <polygon fill="#FFD111" points="41,123 82,82 0,82 " />
+          <rect
+            x="53.009"
+            y="12.009"
+            transform="matrix(0.7071 0.7071 -0.7071 0.7071 53.0087 -45.9742)"
+            fill="#0070BE"
+            width="57.983"
+            height="57.983"
+          />
+          <rect
+            x="94.009"
+            y="53.009"
+            transform="matrix(0.7071 0.7071 -0.7071 0.7071 94.0088 -62.957)"
+            fill="#33BA20"
+            width="57.983"
+            height="57.983"
+          />
+          <polygon fill="#FF4B34" points="0,82 82,82 41,41 " />
+        </svg>
+      </div>
     );
-  }
 
-  return <Element {...props} />;
-};
+    if (this.props.global) {
+      return ReactDOM.createPortal(
+        <Element className="global" />,
+        document.querySelector('body')
+      );
+    }
+
+    return <Element {...this.props} />;
+  }
+}
 
 export default LoadingSpinner;
